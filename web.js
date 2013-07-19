e2b7cc9f9db091a37f5a6aa99c7413951aabffc9
Michael Kapelko <kornerr@gmail.com>
2013-07-19 22:19:15 +0700

    fixed typos, now looks cool

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   buf = fs.readFileSync("index.html");
+  //buf = fs.readFileSync("bitstarter-hw4.html");
   response.send(buf.toString());
 });
 
